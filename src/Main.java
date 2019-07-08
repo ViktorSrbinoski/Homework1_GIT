8f39d91dc9721804c275d2c17605f186dededc72
vsrbinoski <viktor.srbinoski@endava.com>
2019-07-08 11:59:53 +0200

    First branch change 2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Viva Las Vegas");
 
         System.out.println("First branch change 1");
+        System.out.println("First branch change 2");
     }
 }
