2bfe303c5b94ae0daf959e7a170ce0e731eaf117
vsrbinoski <viktor.srbinoski@endava.com>
2019-07-08 11:43:17 +0200

    Change 3, added "Viva Las Vegas" print

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("I am Viktor");
         System.out.println("I like giro");
+        System.out.println("Viva Las Vegas");
     }
 }
